400428b6ad4765fec2f9c3040933998d0666aa7f
0r4cl3 <lucalicheri@gmail.com>
2018-12-08 14:41:32 +0100

    first commit

--- btesting.py
+++ btesting.py
@@ -1,1 +1,13 @@
-import oandapy
+import configparser
+import oandapy as opy
+import pandas as pd
+
+config = configparser.ConfigParser()
+config.read('oanda.cfg')
+
+oanda = opy.API(environment='practice', access_token=config['oanda']['access_token'])
+
+data = oanda.get_history(instrument='EUR_USD', start='2016-12-08', end='2016-12-10', granularity='M1')
+df = pd.DataFrame(data['candles']).set_index('time')
+df.index = pd.DatetimeIndex(df.index)
+df.info()
